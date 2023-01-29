5f1f74a77216415cbfde0cdd6fdcf56819f0a2f7
heonwoochoo <cngjsdn9@gmail.com>
2023-01-29 17:24:05 +0900

    Enemy Init

---
+++ Source/rpg/Public/Enemy/Enemy.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class RPG_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemy();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
