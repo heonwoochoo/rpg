055d5659978a4354df1651a77b5a3411c4a3e497
heonwoochoo <cngjsdn9@gmail.com>
2023-01-31 09:06:36 +0900

    set BlueprintNativeEvent in GetHit function

--- Source/rpg/Private/Breakable/BreakableActor.cpp
+++ Source/rpg/Private/Breakable/BreakableActor.cpp
@@ -23,7 +23,7 @@ void ABreakableActor::Tick(float DeltaTime)
 
 }
 
-void ABreakableActor::GetHit(const FVector& ImpactPoint)
+void ABreakableActor::GetHit_Implementation(const FVector& ImpactPoint)
 {
 
 }
