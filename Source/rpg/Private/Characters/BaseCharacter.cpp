cca9daa69e79c9fc63506692c8f0650f3e81668a
heonwoochoo <cngjsdn9@gmail.com>
2023-02-12 22:43:16 +0900

    Stop enemy attack

--- Source/rpg/Private/Characters/BaseCharacter.cpp
+++ Source/rpg/Private/Characters/BaseCharacter.cpp
@@ -35,6 +35,10 @@ void ABaseCharacter::GetHit_Implementation(const FVector& ImpactPoint, AActor* H
 
 void ABaseCharacter::Attack()
 {
+	if (CombatTarget && CombatTarget->ActorHasTag(FName("Dead")))
+	{
+		CombatTarget = nullptr;
+	}
 }
 
 bool ABaseCharacter::CanAttack()
@@ -53,6 +57,7 @@ void ABaseCharacter::AttackEnd()
 
 void ABaseCharacter::Die()
 {
+	Tags.Add(FName("Dead"));
 	PlayDeathMontage();
 	GetCharacterMovement()->bOrientRotationToMovement = false;
 }
